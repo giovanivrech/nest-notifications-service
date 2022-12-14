4d927d2d28893dc34ccbf44703a4be82ec0e776f
Giovani Vrech <giovanivrech@gmail.com>
2022-12-13 22:24:01 -0300

    feat: create send notification

--- src/application/entities/notification/notification.ts
+++ src/application/entities/notification/notification.ts
@@ -1,6 +1,8 @@
 import { Replace } from 'src/helpers/Replace';
 import { Content } from './content';
 
+import { randomUUID } from 'node:crypto';
+
 export interface NotificationProps {
   recipientId: string;
   content: Content;
@@ -10,13 +12,19 @@ export interface NotificationProps {
 }
 
 export class Notification {
+  private _id: string;
   private props: NotificationProps;
 
   constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._id = randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
     };
+  }
+
+  public get id() {
+    return this._id;
   }
 
   public set recipientId(recipientId: string) {
